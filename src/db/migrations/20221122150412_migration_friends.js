290cb1542b1ab07174263f90f4101c20c1878363
Freak <39245775+KepKepKep@users.noreply.github.com>
2022-11-22 18:44:07 +0200

    fix

--- src/db/migrations/20221122150412_migration_friends.js
+++ src/db/migrations/20221122150412_migration_friends.js
@@ -15,5 +15,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-    return knex.schema.createTable('friends');
+    return knex.schema.dropTable('friends');
 };
