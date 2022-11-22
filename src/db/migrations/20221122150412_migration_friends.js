e7d9472a2e8c3c371227248e8a95a589ffa23f39
Freak <39245775+KepKepKep@users.noreply.github.com>
2022-11-22 18:07:07 +0200

    add/migration friends

---
+++ src/db/migrations/20221122150412_migration_friends.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('friends',(table)=>{
+        table.increments('id');
+        table.integer('subscribe',).references('users.id');
+        table.integer('subscribe_to').references('users.id');
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.createTable('friends');
+};
